15321e1bc753d1f3b94f58cd880e4ffe2c071bac
Shirley Alvarado <shirley@codecrc.com>
2021-04-27 12:06:15 -0600

    data_of_tournament method created in player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,16 @@
 class Player < ApplicationRecord
+  enum gender: [:W, :M]
+  enum title: [:GM, :IM, :FM, :CM, :NM, :WGM, :WIM, :WFM, :WCM]
+  
+  # scope :games_of_tournament, ->(id) { where(player1_id: id) }
+
+  def data_of_tournament(id)
+    Game.where(round_id: Tournament.find(id).rounds.pluck(:id)).where(player1_id: self.id).or(Game.where(round_id: Tournament.find(2).rounds.pluck(:id)).where(player2_id: self.id))
+  end
+
+  def rtng_change
+    self.map
+  end
 end
 
 
