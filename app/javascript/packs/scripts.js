d5db19bd64bde39c2d0b026e3beb75bbb5224355
Marcela Brenes Daniels <marce89455@gmail.com>
2021-04-28 16:34:43 -0600

    some changes made

--- app/javascript/packs/scripts.js
+++ app/javascript/packs/scripts.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-/*!
-    * Start Bootstrap - SB Admin v6.0.2 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    (function($) {
-    "use strict";
-
-    // Add active state to sidbar nav links
-    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-            if (this.href === path) {
-                $(this).addClass("active");
-            }
-        });
-
-    // Toggle the side navigation
-    $("#sidebarToggle").on("click", function(e) {
-        e.preventDefault();
-        $("body").toggleClass("sb-sidenav-toggled");
-    });
-})(jQuery);
-=======
 /*!
  * Start Bootstrap - SB Admin v6.0.2 (https://startbootstrap.com/template/sb-admin)
  * Copyright 2013-2020 Start Bootstrap
@@ -44,4 +20,3 @@
         $("body").toggleClass("sb-sidenav-toggled");
     });
 })(jQuery);
->>>>>>> 0c6deb3859595c46a8af5689ea71e7fcfbe0c0f0
