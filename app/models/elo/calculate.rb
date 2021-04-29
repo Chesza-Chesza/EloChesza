597bdfb22073463376425a49579de59abe518ca6
Shirley Alvarado <shirley@codecrc.com>
2021-04-29 12:51:30 -0600

    Percentage calculation done

--- app/models/elo/calculate.rb
+++ app/models/elo/calculate.rb
@@ -12,22 +12,36 @@ class Elo::Calculate
     data.init
   end
 
-  def hola
-    @player
-  end
-
-
-
   def init
     rating_variation = (@k_value * (@points * @percentage))
   end
 
-  def calculateHigherPercentage
+  def calculateHigherPercentage(difference)
+    a = [3, 10, 17, 25, 32, 39, 46, 53, 61, 68, 76, 83, 91, 98, 106, 113, 121, 129, 137, 145, 153, 162, 170, 179, 188, 197, 206, 215, 225, 235, 245, 256, 267, 278, 290, 302, 315, 328, 344, 357, 374, 391, 411, 432, 456, 484, 517, 559, 619, 735]
+    xpercentage = 0
+     
+    a.each_with_index do |rtng_diff, index|
+      break if rtng_diff >= difference
 
+      number = (0.5 + ((index + 1).to_f / 100))
+      xpercentage = number.truncate(2)
+    end
+
+    xpercentage
   end
 
-  def calculateLowerPercentage
-    
+  def calculateHigherPercentage(difference)
+    a = [3, 10, 17, 25, 32, 39, 46, 53, 61, 68, 76, 83, 91, 98, 106, 113, 121, 129, 137, 145, 153, 162, 170, 179, 188, 197, 206, 215, 225, 235, 245, 256, 267, 278, 290, 302, 315, 328, 344, 357, 374, 391, 411, 432, 456, 484, 517, 559, 619, 735]
+    xpercentage = 0
+     
+    a.each_with_index do |rtng_diff, index|
+      break if rtng_diff >= difference
+
+      number = (0.5 - ((index + 1).to_f / 100))
+      xpercentage = number.truncate(2)
+    end
+
+    xpercentage
   end
 
 end
