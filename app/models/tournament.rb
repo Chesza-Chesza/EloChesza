2665108f576514f5c07c27d6fc80d36a8dcf9399
Marcela Brenes Daniels <marce89455@gmail.com>
2021-04-30 11:00:59 -0600

    some changes

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,11 +1,6 @@
 class Tournament < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :referees
-  has_many :rounds
-=======
   has_and_belongs_to_many :referees
   has_many :rounds
   enum system: [:Swiss, :Round_Robin, :Teams, :Other]
   enum time_control: [:Standard, :Rapid, :Blitz]
->>>>>>> test
 end
