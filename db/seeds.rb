556ec08f79c658d1167fd87c0a5387198c382fdd
Shirley Alvarado <shirley@codecrc.com>
2021-05-03 20:21:48 -0600

    Pg added to production

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,13 @@ Gameplayer.destroy_all
 
 Referee.create!([{ name: 'Alexis Murillo', fide_id: 5678, title: 1 }])
 
-Tournament.create!([{ event_code: 2541, name: 'SJ Open', city: 'San José', country: 'CRC', players_quantity: 8, system: 1, start_date: '12-12-2021', end_date: '12-24-2021', total_rounds: 7, time_control: 1 , referee_id: 1 }])
+Tournament.create!([{ id: 1, event_code: 2541, name: 'SJ Open', city: 'San José', country: 'CRC', players_quantity: 8, system: 1, start_date: '12-12-2021', end_date: '12-24-2021', total_rounds: 7, time_control: 1 , referee_id: 1 }])
 
-Round.create!([{ date: '12-12-2021', number: 1, tournament_id: 1 }])
+Round.create!([
+  { date: '12-12-2021', number: 1, tournament_id: 1 },
+  { date: '13-12-2021', number: 2, tournament_id: 1 },
+  { date: '14-12-2021', number: 3, tournament_id: 1 }
+])
 
 Player.create!([
   { id: 1, fide_number: 1503014, title: 1, name: 'Magnus', last_name: 'Carlsen', fed: 'NOR', elo: 2847, b_day: '01-01-1990', gender: 1, ranked_player: true, k_value: 20, last_elo_update:'04-26-2021' },
@@ -26,8 +30,12 @@ Game.create!([
   { id: 1, player1_id: 1, player2_id: 5, result: 1, won: 1, round_id: 1 },
   { id: 2, player1_id: 2, player2_id: 6, result: 1, won: 1, round_id: 1 },
   { id: 3, player1_id: 3, player2_id: 7, result: 1, won: 1, round_id: 1 },
-  { id: 4, player1_id: 4, player2_id: 8, result: 1, won: 1, round_id: 1 }
+  { id: 4, player1_id: 4, player2_id: 8, result: 1, won: 1, round_id: 1 },
+  { id: 5, player1_id: 1, player2_id: 3, result: 1, won: 1, round_id: 2 },
+  { id: 6, player1_id: 2, player2_id: 4, result: 1, won: 1, round_id: 2 },
+  { id: 7, player1_id: 1, player2_id: 2, result: 1, won: 1, round_id: 3 }
 ])
+
 Gameplayer.create!([
   { player_id: 1, elo: 2847, rtng_change: 0, game_id: 1 },
   { player_id: 2, elo: 2820, rtng_change: 0, game_id: 2 },
