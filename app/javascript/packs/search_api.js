064e633f8e2118ec2a2c3ec3cc243eb51485723b
Shirley Alvarado <shirley@codecrc.com>
2021-05-10 23:26:04 -0600

    Merge branch test

--- app/javascript/packs/search_api.js
+++ app/javascript/packs/search_api.js
@@ -1,22 +1,4 @@
 async function fetchPlayerData() {
-<<<<<<< HEAD
-    console.log('Fuera 1')
-    const button = document.getElementById('top10')
-    console.log(button)
-    button.addEventListener('click', async function(){
-        const data_raw = await fetch('/players?top10=true', {
-        headers:  {
-            "Content-Type": "application/javascript",
-            "Accept": "application/javascript"
-          }
-        }
-      )
-        console.log('sal por favor')
-    })
-}
-document.addEventListener('turbolinks:load', fetchPlayerData)
-console.log('Fuera')
-=======
     const data_raw = await fetch('https://fide-ratings.herokuapp.com/api/players')
     const data = await data_raw.json()
 
@@ -31,4 +13,3 @@ console.log('Fuera')
     players_data.innerText = players
 }
 document.addEventListener('turbolinks:load', fetchPlayerData)
->>>>>>> main
